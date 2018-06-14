0f4b11fb31d546f701c6c1ac8f56c822839d204f
mohamed.aly.omer@gmail.com <Mohamed!983>
2018-06-14 04:41:00 +0200

    Add trteeb

--- SyndicateService/SyndicateService/Datasource/dsETSMobile1.Designer.cs
+++ SyndicateService/SyndicateService/Datasource/dsETSMobile1.Designer.cs
@@ -13238,8 +13238,8 @@ FROM            TblMemberAmanat";
             this._commandCollection[0] = new global::System.Data.SqlClient.SqlCommand();
             this._commandCollection[0].Connection = this.Connection;
             this._commandCollection[0].CommandText = "SELECT        MMashatId\r\nFROM            TBLMashat\r\nWHERE        (hafzano = @hafz" +
-                "ano) AND (Activate <> 1) AND (ActivateFromMobile IS NULL OR ActivateFromMobile =" +
-                " 0)";
+                "ano) AND (Activate <> 1) AND (ActivateFromMobile IS NULL OR\r\n                   " +
+                "      ActivateFromMobile = 0)\r\nORDER BY trteep";
             this._commandCollection[0].CommandType = global::System.Data.CommandType.Text;
             this._commandCollection[0].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@hafzano", global::System.Data.SqlDbType.Int, 4, global::System.Data.ParameterDirection.Input, 0, 0, "hafzano", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
         }
